4fef55b75ec4814b06f3f072901f917e6b11fccb
grishinrv <roman1988grishin@gmail.com>
2022-03-20 19:59:22 +0300

    Error model

---
+++ src/WebApi/WebApi/Models/Error.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApi.Models;
+
+public record Error
+{
+    public Error(ErrorType type)
+    {
+        switch (type)
+        {
+            case Models.ErrorType.ParameterIsMissing:
+                ErrorType = "Parameter is missing";
+                break;
+            case Models.ErrorType.ExternalServiceError:
+                ErrorType = "External service error";                
+                break;
+            default:
+                ErrorType = "Undefined";
+                break;
+        }
+    }
+
+    public string ErrorType { get; }
+
+    public string Description { get; init; }
+}
---
+++ src/WebApi/WebApi/Models/ErrorType.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Models;
+
+public enum ErrorType
+{
+    ParameterIsMissing = 0,
+    ExternalServiceError = 1,
+    
+}
