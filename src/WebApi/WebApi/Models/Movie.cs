51abf27e7fc59e07446725717198b41ab2ebd639
grishinrv <roman1988grishin@gmail.com>
2022-03-21 13:37:25 +0300

    Movie model fix

--- src/WebApi/WebApi/Models/Movie.cs
+++ src/WebApi/WebApi/Models/Movie.cs
@@ -1,60 +1,87 @@
-﻿namespace WebApi.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace WebApi.Models;
 
 public record Movie
 {
+    [JsonPropertyName("Title")]
     public string Title { get; init; }
-    public int Year { get; init; }
+    [JsonPropertyName("Year")]
+    public string Year { get; init; }
+    [JsonPropertyName("Rated")]
     public string Rated { get; init; }
-    public string Released { get; init; }
+    [JsonPropertyName("Released")]
+    public DateOnly Released { get; init; }
+    [JsonPropertyName("Runtime")]
     public string Runtime { get; init; }
+    [JsonPropertyName("Plot")]
     public string Plot { get; init; }
     /// <summary>
     /// Genre
     /// </summary>
-    public List<string> Genres { get; init; }
+    [JsonPropertyName("Genre")]
+    public string Genres { get; init; }
+    [JsonPropertyName("Director")]
     public string Director { get; init; }
     /// <summary>
     /// Writer
     /// </summary>
-    public List<string> Writers { get; init; }
-    public List<string> Actors { get; init; }
-    public string Language { get; init; }
+    [JsonPropertyName("Writer")]
+    public string Writers { get; init; }
+    [JsonPropertyName("Actors")]
+    public string Actors { get; init; }
+    [JsonPropertyName("Language")]
+    public string Languages { get; init; }
     /// <summary>
     /// Country
     /// </summary>
-    public List<string> Countries { get; init; }
-    public List<string> Awards { get; init; }
+    [JsonPropertyName("Country")]
+    public string Countries { get; init; }
+    [JsonPropertyName("Awards")]
+    public string Awards { get; init; }
     /// <summary>
     /// Poster image
     /// </summary>
+    [JsonPropertyName("Poster")]
     public string Poster { get; init; }
-    public List<string> Ratings { get; init; }
+    [JsonPropertyName("Ratings")]
+    public List<Rating> Ratings { get; init; }
     /// <summary>
     /// Metascore
     /// </summary>
+    [JsonPropertyName("Metascore")]
     public string MetaScore { get; init; }
     /// <summary>
     /// imdbRating
     /// </summary>
+    [JsonPropertyName("imdbRating")]
     public string ImdbRating { get; init; }
     /// <summary>
     /// imdbVotes
     /// </summary>
+    [JsonPropertyName("imdbVotes")]
     public string ImdbVotes { get; init; }
     /// <summary>
     /// imdbID
     /// </summary>
+    [JsonPropertyName("imdbID")]
     public string ImdbId { get; init; }
     /// <summary>
     /// Type
     /// </summary>
+    [JsonPropertyName("Type")]
     public string MovieType { get; init; }
     /// <summary>
     /// DVD
     /// </summary>
-    public string Dvd { get; init; }
+    [JsonPropertyName("DVD")]
+    public DateOnly DvdReleaseDate { get; init; }
+    [JsonPropertyName("BoxOffice")]
     public string BoxOffice { get; init; }
+    [JsonPropertyName("Production")]
     public string Production { get; init; }
+    [JsonPropertyName("Website")]
     public string Website { get; init; }
+    [JsonPropertyName("Response")]
     public bool Response { get; init; }
 }
---
+++ src/WebApi/WebApi/Models/Rating.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApi.Models;
+
+public record Rating
+{
+    [JsonPropertyName("Source")]
+    public string Source { get; init; }
+    [JsonPropertyName("Value")]
+    public string Value { get; init; }
+}
