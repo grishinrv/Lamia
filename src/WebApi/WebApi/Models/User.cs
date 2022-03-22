61e64b5b6682a211abbfb7199c954e851c697f8d
grishinrv <roman1988grishin@gmail.com>
2022-03-22 08:41:05 +0300

    TokenService implementation

---
+++ src/WebApi/WebApi/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApi.Models;
+
+public record User
+{
+    [JsonPropertyName("UserName")]
+    public string UserName { get; init; }
+    [JsonPropertyName("Password")]
+    public string Password { get; init; }
+}
---
+++ src/WebApi/WebApi/Services/ITokenService.cs
@@ -0,0 +1,23 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Services;
+
+public interface ITokenService
+{
+    /// <summary>
+    /// Is used to build the token.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="issuer"></param>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    string BuildToken(string key, string issuer, User user);
+    /// <summary>
+    /// Is used to check whether a given token is valid.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="issuer"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    bool IsTokenValid(string key, string issuer, string token);
+}
