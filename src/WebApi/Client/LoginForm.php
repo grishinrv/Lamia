899bc9289def5c9de4e86e08c5d9897e29686016
grishinrv <roman1988grishin@gmail.com>
2022-03-23 08:36:56 +0300

    php client draft

---
+++ src/WebApi/Client/LoginForm.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Login into Lamia api</title>
+        <meta charset='utf-8'>
+    </head>
+    <body>
+        <?php
+            echo "Hello";
+        ?>
+    </body>
+</html>
+
